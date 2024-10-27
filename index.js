b6b5df0ace2f611ab2bfc0a62f1751f0bad6e959
Nazia Ali <naziaaliburong@gmail.com>
2024-10-27 22:06:23 +0500

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
